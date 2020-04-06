53b380fbc4fc50b60b14792971c16e6c7d49a726
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-06 21:22:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,6 @@ function accessElementInArray(array,index){
 };
 
 function destructivelyRemoveElementFromBeginningOfArray(array,element){
-  return array.shift(element);
-}
+  array.shift(element);
+  return array;
+};
