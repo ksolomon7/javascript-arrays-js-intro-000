b4d6ac4b9679715cecdc0bcc878ee6e12426ad4e
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-06 21:28:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 };
 
 function removeElementFromBeginningOfArray(array){
-  array.slice([0]);
-  return array;
+  let newArray=array.slice(1);
+  return newArray;
 }
