cb448257b4918752e54fe909819dbe3d4b2c0231
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-06 21:03:14 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars= ["snickers", "hundred grand", "kitkat","skittles"];
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array
+}
+
+function addElementToBeginningOfArray(array,element){
+  let newArray= [element,...array];
+  return newArray;
+}
